c5f74ce47f949f1c0748fedb210469e4bc1af8ae
Ben Parsons <9parsonsb@gmail.com>
2025-02-12 17:26:00 +1100

    Backend done

--- Contact.cs
+++ Contact.cs
@@ -2,6 +2,10 @@ namespace WebApi;
 
 public class Contact
 {
+    /// <summary>
+    /// Empty/Null = not saved
+    /// </summary>
+    public Guid? Id { get; set; } = Guid.Empty;
     public required string FirstName { get; init; }
     public required string LastName { get; init; }
     public required string Company { get; init; }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
+using Lamar.Microsoft.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseLamar((_, registry) => registry.Scan(scan =>
+{
+    scan.AssemblyContainingType<Program>();
+    scan.WithDefaultConventions();
+    scan.LookForRegistries();
+}));
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ContactDbContext>();
 
 var app = builder.Build();
 
@@ -19,25 +29,29 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// stuff goes here
-builder.Services.AddDbContext<ContactDbContext>();
-
 // TODO: Validation
 // TODO: Auth
 // TODO: Web Security
 // TODO: Dont use Domain Models as API Models
-app.MapGet("/", async (string query, ContactDbContext db) => await db.Contacts.Where(c => EF.Functions.Like(c.FirstName.Trim(), $"%{query}%") ||
+app.MapGet("/", async ([FromServices] ContactDbContext db, string query = "") => await db.Contacts.Where(c => EF.Functions.Like(c.FirstName.Trim(), $"%{query}%") ||
     EF.Functions.Like(c.LastName.Trim(), $"%{query}%") ||
     EF.Functions.Like(c.Company.Trim(), $"%{query}%") ||
     EF.Functions.Like(c.Email.Trim(), $"%{query}%")
     ).AsNoTracking().ToListAsync());
-app.MapGet("/{id:int}", async (ContactDbContext db, int id) => await db.Contacts.FindAsync(id));
-app.MapPost("/", async (ContactDbContext db, Contact contact) => await db.Contacts.AddAsync(contact));
-app.MapPut("/", async (ContactDbContext db, Contact contact) =>
+app.MapGet("/{id:guid}", async ([FromServices] ContactDbContext db, Guid id) => await db.Contacts.FindAsync(id));
+app.MapPost("/", async ([FromServices] ContactDbContext db, Contact contact) =>
+{
+    contact.Id = Guid.NewGuid();    
+    await db.Contacts.AddAsync(contact);
+    await db.SaveChangesAsync();
+    return contact.Id;
+});
+app.MapPut("/", async ([FromServices] ContactDbContext db, Contact contact) =>
 {
     db.Entry(contact).State = EntityState.Modified;
     await db.SaveChangesAsync();
 });
 
+await app.Services.GetRequiredService<ContactDbContext>().Database.EnsureCreatedAsync();
 
-app.Run();
+await app.RunAsync();
